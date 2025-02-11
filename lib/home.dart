cf742d82f0dc8d53b14883593029c3efbd2f9e49
Rustem <r132420@gmail.com>
2025-02-12 00:06:21 +0300

    + appbar title

--- lib/home.dart
+++ lib/home.dart
@@ -5,7 +5,10 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CookBook'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
